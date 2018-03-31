1e8d8c0495dcbb34d90995e979e5b96c46e69caa
banana <renon@disroot.org>
2018-03-31 10:11:33 +0200

    done

--- mastExt.py
+++ mastExt.py
@@ -5,10 +5,11 @@ import sys
 import os
 import math
 import skimage
-from skimage import feature
+from skimage import feature as skft
+import pickle
 
-path = str(sys.argv[1])
-
+mnistPath = str(sys.argv[1])
+cifarPath = str(sys.argv[2])
 
 def lecture_mnist(path):
 
@@ -16,12 +17,21 @@ def lecture_mnist(path):
     DataApp, LabelApp = mdata.load_training()
     DataTest, LabelTest = mdata.load_testing()
     
-    # DataApp et DataTest sont des listes contenant elles-memes des listes, ces dernieres
-    # etant les donnees images
-    # LabelApp et LabelTest sont des type array contenant la classe des donnees de meme indice
-    # dans DataApp et DataTest 		
-
     return np.array(DataApp+DataTest) , np.array(LabelApp+LabelTest)
+
+
+def lecture_cifar(path):
+    X, Y = None, None
+
+    for file in os.listdir(path):
+
+        with open(os.path.join(path, file), 'rb') as cifile:
+            dict = pickle.load(cifile, encoding='latin1')
+
+            X = X + dict['data'] if type(X)==None else dict['data']
+            Y = Y + dict['labels'] if type(Y)==None else dict['labels']
+
+    return X,Y
 
 
 def decoupage_donnees(X,Y):
@@ -41,179 +51,135 @@ def decoupage_donnees(X,Y):
 
 def kppv_distances(Xtest, Xapp):        
         
-        appN = np.tile(np.linalg.norm(Xapp, axis=1)**2,(len(Xtest),1))
-        testN = np.tile(np.linalg.norm(Xtest, axis=1)**2,(len(Xapp),1))
-        
-        prod = 2*np.matmul(Xtest, Xapp.transpose())
-        
-        return appN - prod + testN.transpose()
-
-#def kppv_distances(Xtest, Xapp):
-#
-#    norm = lambda x,y,z : np.matmul(x, y.transpose())
-#
-#    return norm(Xapp,Xapp,0) + norm(Xtest,Xtest,1).transpose() - 2*np.matmul(Xapp,Xtest.transpose()).transpose()
+    appN = np.tile(np.linalg.norm(Xapp, axis=1)**2,(len(Xtest),1))
+    testN = np.tile(np.linalg.norm(Xtest, axis=1)**2,(len(Xapp),1))
+    
+    prod = 2*np.matmul(Xtest, Xapp.transpose())
+    
+    return appN - prod + testN.transpose()
 
 
 def kppv_predict(Dist, Yapp, K):
+
+    bestKIndex = Dist.argsort()[:,:K]
+    bestKClasses = Yapp[bestKIndex]
+    YPred =[]
+
+    for bests in bestKClasses:
+        d = defaultdict(int)
+        for i in bests:
+            d[i] += 1
+        result = max(d.items(), key=lambda x: x[1])
+        YPred.append(result[0])
+
+    return YPred
         
-        Ypred = np.arange(len(Dist))
-        
-        for l in range(len(Dist)):
-                Indices = np.argsort(Dist[l,:])
-                
-                YappKeepKClosest = Yapp[Indices[0:K]]
-                
-                ClassCounts = np.bincount(YappKeepKClosest)
-                
-                MajorityClass =  np.argmax(ClassCounts)
-                
-                Ypred[l] = MajorityClass
 
-        print(Ypred, Ypred.shape())
-
-
-        bestKIndex = Dist.argsort()[:,:K]
-        bestKClasses = Yapp[bestKIndex]
-
-        #print(Dist,Dist[0,bestKIndex],Yapp,Yapp[bestKIndex])
-        beef =[]
-
-        for bests in bestKClasses:
-                d = defaultdict(int)
-                for i in bests:
-                        d[i] += 1
-
-                result = max(d.items(), key=lambda x: x[1])
-                beef.append(result[0])
-
-        print(np.asarray(beef), np.asarray(beef).shape())
-
-        return Ypred
-
-def evaluation_classifieur(Ytest, Ypred):
-        Count = np.count_nonzero(Ytest==Ypred)
-        Accuracy = Count / len(Ytest)
-        return Accuracy
-
-MnistX, MnistY = lecture_mnist(path)
-
-print(" ")
-print("X and Y shapes")
-print(np.shape(MnistX))
-print(np.shape(MnistY))
-print(" ")
-
-MnistXapp, MnistYapp, MnistXtest, MnistYtest = decoupage_donnees(MnistX, MnistY)
-
-print("Mnist app and test shapes")
-print(np.shape(MnistXapp))
-print(np.shape(MnistYapp))
-print(np.shape(MnistXtest))
-print(np.shape(MnistYtest))
-print(" ")
-
-MnistXappReduit = MnistXapp[0:1000,:]
-MnistYappReduit = MnistYapp[0:1000]
-MnistXtestReduit = MnistXtest[:,:]
-MnistYtestReduit = MnistYtest[:]
-
-MnistDist = kppv_distances(MnistXtestReduit, MnistXappReduit)
-
-KMax = 5
-
-for K in range(1,KMax):
-
-	MnistPredictions = kppv_predict(MnistDist, MnistYappReduit, K)
-
-	Accuracy = evaluation_classifieur(MnistYtestReduit, MnistPredictions)
-	print("K = ", K, "Accuracy : ", Accuracy)
-print(" ")
+evaluation_classifieur=lambda test,pred: np.count_nonzero(test==pred)/len(test)
 
 
 def ExtractFeatures(X,n,m,c):
-        HOG = X
-        LBP = X
-        
-        for i in range(0,len(X)):
-                image = X[i,:].reshape(n,m)
-                P = 5
-                R = 2.2
-                HOG[i,0:81] = skimage.feature.hog(image, orientations=9, pixels_per_cell=(8, 8), cells_per_block=(3, 3)).flatten()
-                HOG[i,81:] = np.zeros((1,703))
-                LBP[i,:] = skimage.feature.local_binary_pattern(image, P, R, method='default').flatten()
-        
-        return HOG, LBP
-        
-K = 5
+    HOG, LBP = np.empty(0), np.empty(0)
+    
+    for i in range(0,len(X)):
+        im = X[i,:].reshape(n,m,c)
 
-MnistHOGapp, MnistLBPapp = ExtractFeatures(MnistXappReduit,28,28,1)
-MnistHOGtest, MnistLBPtest = ExtractFeatures(MnistXtestReduit,28,28,1)
+        im = np.mean(im, axis = 2)
+        R = 1
+        P = R*8
 
-# HOG Features
+        HOGi = skft.hog(im)[np.newaxis,:]
+        LBPi = skft.local_binary_pattern(im, P, R).flatten()[np.newaxis,:]
 
-MnistHOGDist = kppv_distances(MnistHOGtest, MnistHOGapp)
-MnistHOGPred = kppv_predict(MnistHOGDist, MnistYappReduit, K)
-print(MnistHOGPred)
-MnistHOGAccuracy = evaluation_classifieur(MnistYtestReduit, MnistHOGPred)
+        HOG = np.concatenate((HOG,HOGi)) if HOG.size else HOGi
+        LBP = np.concatenate((LBP,LBPi)) if LBP.size else LBPi
 
-print(MnistHOGAccuracy)
-
-MnistLBPDist = kppv_distances(MnistLBPtest, MnistLBPapp)
-MnistLBPPred = kppv_predict(MnistLBPDist, MnistYapp, K)
-MnistLBPAccuracy = evaluation_classifieur(MnistYtest, MnistLBPPred)
-
-print(MnistLBPAccuracy)
-
+    return HOG, LBP
 
 # Cross-Validation
 
-def create_folds(X, Y, n):
-        X_list = []
-        Y_list = []
-        
-        for i in range (1,n):
-                X_list.append(X[i:,:]) # Change !
-                Y_list.append(Y[i:])
-        
-        #return X_listapp, Y_listapp, X_listtest, Y_listtest
-        return X_list, Y_list, X_list, Y_list
+create_folds = lambda X,Y,n : (np.split(X,n), np.split(Y,n))
 
-def kppv_distances_forEachFold(X_listtest, X_listapp):
-        Dist_list = []
-        numFolds = len(X_listtest)
+def kppv_pred_Xfold(X_list, Y_list, K):
+        Pred_list = []
+        numFolds = len(X_list)
         
         for i in range(0,numFolds):
-                Dist_list.append(kppv_distances(X_listtest[i], X_listapp[i]))
-        
-        return Dist_list
+                X_applist = [i for i in X_list]
+                Y_applist = [i for i in Y_list]
 
-def kppv_predict_forEachFold(Dist_list, Y_listapp, K):
-        Y_pred_list = []
-        numFolds = len(Y_listapp)
+                X_test = X_list[i]
+
+                X_applist.pop(i)
+                Y_applist.pop(i)
+
+                X_app = np.concatenate(X_applist)
+                Y_app = np.concatenate(Y_applist)
+
+                distI = kppv_distances(X_test, X_app)
+
+                Pred_list.append(kppv_predict(distI, Y_app, K))
+
+        return Pred_list
+
+
+def evaluation_Xfold(Y_list, Pred_list):
+        Acc = 0
+        numFolds = len(Y_list)
         
         for i in range(0,numFolds):
-                Y_pred_list.append(kppv_predict(Dist_list[i], Y_listapp[i], K))
+                Acc += evaluation_classifieur(Y_list[i], Pred_list[i])
         
-        return Y_pred_list
+        return round(Acc/numFolds,3)
 
-def evaluation_CV(Y_listtest, Y_pred_list):
-        Accuracy = 0
-        numFolds = len(Y_listtest)
+
+
+if __name__=='__main__':
+
+    mnistX, mnistY = lecture_mnist(mnistPath)
+    cifarX, cifarY = lecture_cifar(cifarPath)
+    
+    mnistXapp, mnistYapp, mnistXtest, mnistYtest = decoupage_donnees(mnistX, mnistY)
+    
+    mnistXappT = mnistXapp[0:500,:]
+    mnistYappT = mnistYapp[0:500]
+
+    mnistXtestT = mnistXtest[0:500,:]
+    mnistYtestT = mnistYtest[0:500]   
+
+    mnistDist = kppv_distances(mnistXtestT, mnistXappT)
+    
+    #Testing with different number of neighbours
+    for K in range(1,8):
+    
+        mnistPred = kppv_predict(mnistDist, mnistYappT, K)
+        mnistAcc = evaluation_classifieur(mnistYtestT, mnistPred)
+
+        print(K,"-nn :")
+        print(mnistAcc)
+
+        mnistHOGapp, mnistLBPapp = ExtractFeatures(mnistXappT,28,28,1)
+        mnistHOGtest, mnistLBPtest = ExtractFeatures(mnistXtestT,28,28,1)
+
+        mnistHOGDist = kppv_distances(mnistHOGtest, mnistHOGapp)
+        mnistHOGPred = kppv_predict(mnistHOGDist, mnistYappT, K)
+        mnistHOGAcc = evaluation_classifieur(mnistYtestT, mnistHOGPred)
         
-        for i in range(0,numFolds):
-                Accuracy += evaluation_classifieur(Y_listtest[i], Ypred[i])
+        print(mnistHOGAcc,"(HOG)")
         
-        Accuracy = Accuracy / numFolds
+        mnistLBPDist = kppv_distances(mnistLBPtest, mnistLBPapp)
+        mnistLBPPred = kppv_predict(mnistLBPDist, mnistYappT, K)
+        mnistLBPAcc = evaluation_classifieur(mnistYtestT, mnistLBPPred)
         
-        return Accuracy
+        print(mnistLBPAcc,"(LBP)")
 
-n = 10
-K = 5
 
-MnistX_listapp, MnistY_listapp, MnistX_listtest, MnistY_listtest = create_folds(MnistX[0:1000,:],MnistY[0:1000], n)
-MnistDist_list = kppv_distances_forEachFold(MnistX_listtest, MnistX_listapp)
-MnistY_pred_list = kppv_predict_forEachFold(MnistDist_list, MnistY_listapp, K)
-MnistCVAccuracy = evaluation_CV(MnistY_listtest, MnistY_pred_list)
+    #Cross Validation
 
-print(MnistCVAccuracy)
+    Nfolds = 10
+    Knn = 5
+    print("Testing",Nfolds,"-fold X Validation on",K,"-nn:")
+
+    mnistX_list, mnistY_list = create_folds(mnistX[0:500,:],mnistY[0:500], Nfolds)
+    mnistPred_list = kppv_pred_Xfold(mnistX_list, mnistY_list, Knn)
+    print(evaluation_Xfold(mnistY_list, mnistPred_list))
